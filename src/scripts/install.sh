ea0535348b28b97ac5bbee02254e7317d767bcc1
Justin Burr <20321959+juburr@users.noreply.github.com>
2024-08-05 09:48:42 -0400

    fix: drop sudo requirement and bump default apko version (#5)
    Co-authored-by: Justin Burr <juburr@users.noreply.github.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -2,12 +2,25 @@
 
 set -e
 
+# Ensure CircleCI environment variables can be passed in as orb parameters
+INSTALL_PATH=$(circleci env subst "${PARAM_INSTALL_PATH}")
+VERSION=$(circleci env subst "${PARAM_VERSION}")
+
+# Check if the apko tar file was in the CircleCI cache.
+# Cache restoration is handled in install.yml
 if [[ -f apko.tar.gz ]]; then
-    tar zxvf apko.tar.gz "apko_${PARAM_VERSION}_linux_amd64/apko" --strip 1
+    tar zxvf apko.tar.gz "apko_${VERSION}_linux_amd64/apko" --strip 1
 fi
+
+# If there was no cache hit, go ahead and re-download the binary.
 if [[ ! -f apko ]]; then
-    wget "https://github.com/chainguard-dev/apko/releases/download/v${PARAM_VERSION}/apko_${PARAM_VERSION}_linux_amd64.tar.gz" -O apko.tar.gz
-    tar zxvf apko.tar.gz "apko_${PARAM_VERSION}_linux_amd64/apko" --strip 1
+    wget "https://github.com/chainguard-dev/apko/releases/download/v${VERSION}/apko_${VERSION}_linux_amd64.tar.gz" -O apko.tar.gz
+    tar zxvf apko.tar.gz "apko_${VERSION}_linux_amd64/apko" --strip 1
 fi
-sudo mv apko /usr/local/bin/apko
-sudo chmod +x /usr/local/bin/apko
+
+# An apko binary should exist at this point, regardless of whether it was obtained
+# through cache or re-downloaded. Move it to an appropriate bin directory and mark it
+# as executable. If your pipeline throws an error here, you may want to choose an
+# INSTALL_PATH that doesn't require sudo access, so this orb can avoid any root actions.
+mv apko "${INSTALL_PATH}/apko"
+chmod +x "${INSTALL_PATH}/apko"
